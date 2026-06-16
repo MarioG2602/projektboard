8f3329c496eb26f723d77dd7e64a36ecaa207bc0
MarioG2602 <Mario_0302@yahoo.de>
2026-06-16 15:52:02 +0200

    Keep users signed in with session refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "projektboard-v19";
+const CACHE_NAME = "projektboard-v20";
 const APP_SHELL = ["./", "./index.html", "./data-core.js", "./sync-core.js", "./manifest.webmanifest", "./icons/projektboard-192.png", "./icons/projektboard-512.png"];
 const CACHEABLE_DESTINATIONS = new Set([
   "document",
